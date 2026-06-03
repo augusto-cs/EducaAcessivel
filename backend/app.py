38204ccc9ea8c382e8d31f26c6695dd0b9ca2d2b
augusto <marquinaugustocruz@gmail.com>
2026-06-03 11:55:08 -0300

    ajuste nome do modelo para gemini-1.5-flash-latest

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_folder='static')
 CORS(app)
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-pro')
 
 STATIC_DIR = os.path.join(os.getcwd(), 'static')
 if not os.path.exists(STATIC_DIR):
